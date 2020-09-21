7f0f95c532a281f906923ea61e53441840e4bd3f
Patrick Christ <patrick.christ.91@web.de>
2020-09-21 21:33:30 +0200

    Add web filter

--- backend/src/main/kotlin/de/pcp11/startups/config/SecurityConfig.kt
+++ backend/src/main/kotlin/de/pcp11/startups/config/SecurityConfig.kt
@@ -1,11 +1,14 @@
 package de.pcp11.startups.config
 
 import org.springframework.context.annotation.Bean
-import org.springframework.security.config.Customizer.withDefaults
+import org.springframework.security.authentication.ReactiveAuthenticationManager
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter
+import org.springframework.security.web.server.authentication.ServerAuthenticationConverter
 
 
 @EnableWebFluxSecurity
@@ -13,14 +16,24 @@ import org.springframework.security.web.server.SecurityWebFilterChain
 class SecurityConfig {
 
     @Bean
-    fun springWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+    fun springWebFilterChain(http: ServerHttpSecurity,
+                             jwtAuthenticationManager: ReactiveAuthenticationManager,
+                             jwtAuthenticationConverter: ServerAuthenticationConverter): SecurityWebFilterChain {
+        val authenticationWebFilter = AuthenticationWebFilter(jwtAuthenticationManager)
+        authenticationWebFilter.setServerAuthenticationConverter(jwtAuthenticationConverter)
+
         return http
                 .authorizeExchange { exchanges ->
                     exchanges.pathMatchers("/api/startups").permitAll()
                     exchanges.pathMatchers("/api/startup/**").permitAll()
+                    exchanges.pathMatchers("/api/auth/**").permitAll()
                     exchanges.anyExchange().authenticated()
                 }
-                .httpBasic(withDefaults())
+                .addFilterAt(authenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+                .httpBasic().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .logout().disable()
                 .build();
     }
 }
