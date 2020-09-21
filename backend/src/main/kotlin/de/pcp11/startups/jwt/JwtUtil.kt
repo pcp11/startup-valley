cec1fb740d3d117d9c8f3258d20d8c7d55f2ab47
Patrick Christ <patrick.christ.91@web.de>
2020-09-21 21:32:12 +0200

    Implement jwt util

---
+++ backend/src/main/kotlin/de/pcp11/startups/jwt/JwtUtil.kt
@@ -0,0 +1,35 @@
+package de.pcp11.startups.jwt
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jws
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import org.springframework.stereotype.Service
+import java.security.KeyPair
+import java.time.Duration
+import java.time.Instant
+import java.util.*
+
+@Service
+class JwtUtil {
+
+    val keyPair: KeyPair = Keys.keyPairFor(SignatureAlgorithm.RS256)
+
+    fun createJwt(userId: String): String {
+        return Jwts.builder()
+                .signWith(keyPair.private, SignatureAlgorithm.RS256)
+                .setSubject(userId)
+                .setIssuer("identity")
+                .setExpiration(Date.from(Instant.now().plus(Duration.ofMinutes(30))))
+                .setIssuedAt(Date.from(Instant.now()))
+                .compact()
+    }
+
+    fun validateJwt(jwt: String): Jws<Claims> {
+        return Jwts.parserBuilder()
+                .setSigningKey(keyPair.public)
+                .build()
+                .parseClaimsJws(jwt)
+    }
+}
