3d7c4e7e268af9894eb4e15178c0709272a1f8b0
Patrick Christ <patrick.christ.91@web.de>
2020-09-26 21:16:11 +0200

    Refactor security config

--- backend/src/main/kotlin/de/pcp11/startups/config/SecurityConfig.kt
+++ backend/src/main/kotlin/de/pcp11/startups/config/SecurityConfig.kt
@@ -1,19 +1,51 @@
 package de.pcp11.startups.config
 
+import de.pcp11.startups.jwt.JwtUtil
 import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.ReactiveAuthenticationManager
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 class SecurityConfig {
+
+    @Bean
+    fun jwtAuthenticationConverter(): ServerAuthenticationConverter {
+        return ServerAuthenticationConverter { exchange ->
+            Mono.justOrEmpty(exchange)
+                    .flatMap { Mono.justOrEmpty(it.request.cookies["X-Auth"]) }
+                    .filter { it.isNotEmpty() }
+                    .map { it[0].value }
+                    .map { UsernamePasswordAuthenticationToken(it, it) }
+        }
+    }
+
+    @Bean
+    fun jwtAuthenticationManager(jwtUtil: JwtUtil): ReactiveAuthenticationManager {
+        return ReactiveAuthenticationManager { authentication ->
+            Mono.justOrEmpty(authentication)
+                    .map { jwtUtil.validateJwt(it.credentials as String) }
+                    .map { jws ->
+                        UsernamePasswordAuthenticationToken(
+                                jws.body.subject,
+                                authentication.credentials as String,
+                                mutableListOf(SimpleGrantedAuthority("ROLE_USER"))
+                        )
+                    }
+        }
+    }
 
     @Bean
     fun springWebFilterChain(http: ServerHttpSecurity,
@@ -23,9 +55,14 @@ class SecurityConfig {
         authenticationWebFilter.setServerAuthenticationConverter(jwtAuthenticationConverter)
 
         return http
+                .exceptionHandling()
+                .authenticationEntryPoint { swe, _ -> { swe.response.setStatusCode(HttpStatus.UNAUTHORIZED) }.toMono().then() }
+                .accessDeniedHandler { swe, _ -> { swe.response.setStatusCode(HttpStatus.FORBIDDEN) }.toMono().then() }
+                .and()
                 .authorizeExchange { exchanges ->
                     exchanges.pathMatchers("/api/startups").permitAll()
                     exchanges.pathMatchers("/api/startup/**").permitAll()
+                    exchanges.pathMatchers("/api/secured/**").authenticated()
                     exchanges.pathMatchers("/api/auth/**").permitAll()
                     exchanges.anyExchange().authenticated()
                 }
@@ -34,6 +71,6 @@ class SecurityConfig {
                 .csrf().disable()
                 .formLogin().disable()
                 .logout().disable()
-                .build();
+                .build()
     }
 }
